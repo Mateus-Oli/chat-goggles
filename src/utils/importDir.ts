e64a00861804d5e47b1938eb7978105903e73c68
Mateus <mateus.oli.car@gmail.com>
2018-02-20 01:34:39 -0300

    remove promisify

--- src/utils/importDir.ts
+++ src/utils/importDir.ts
@@ -1,8 +1,9 @@
 import { readdir } from "fs";
-import { promisify } from "util";
 
+const readDir = (dir: string) => new Promise<string[]>((resolve, reject) =>
+  readdir(dir, (error, files) => error ? Promise.reject(error) : Promise.resolve(files)));
 
-export const importDir = (dir: string) => promisify(readdir)(dir)
+export const importDir = (dir: string) => readDir(dir)
   .then(files => files.filter(file => file.match(/\.[t|j]sx?$/)))
   .then(files => files.map(file => `${dir.replace(/\/*$/, '')}/${file}`))
   .then(files => files.map(file => import(file)))
